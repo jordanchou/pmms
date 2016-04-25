fda9735d97dbd0d33e8818891f7a872e045ede53
jordan <jordan.chou1@gmail.com>
2016-04-25 11:00:34 +0800

    Added testing script

---
+++ Process/anything.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# COMPILE PROGRAM
+make multiProcess
+
+# RUN PROGRAM FOR COMBINATION OF M N K VALUES
+# FROM 1 TO 100
+for i in {100..1};
+do
+	for j in {100..1};
+	do
+		for k in {100..1};
+		do
+			echo $i $j $k
+			./multiProcess ma2.txt ma2.txt $i $j $k
+		done
+	done
+done
