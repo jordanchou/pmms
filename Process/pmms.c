c2d7d8812e40ea6426862157e8c57bb043c567c5
jordan <jordan.chou1@gmail.com>
2016-05-08 20:22:15 +0800

    More report stuff

--- Process/pmms.c
+++ Process/pmms.c
@@ -61,7 +61,7 @@ int main(int argc, char** argv)
     semaphores[2] = sem_empty;
 
     //Create child processes
-    process_num = -1;//First process will be 0
+    process_num = 0;
     pid = -1;
 
     //Creat M children
